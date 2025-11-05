9482a6797fb9fe4d84d6d14aa73c4016054189fe
Freeman Jiang <freeman.jiang.ca@gmail.com>
2025-11-05 01:19:10 -0500

    fix scripts

--- pm2.config.js
+++ pm2.config.js
@@ -1,6 +1,6 @@
 // See: https://bun.com/docs/guides/ecosystem/pm2
 module.exports = {
-  title: "beatsync-server", // Name of your application
+  name: "beatsync-server", // Name of your application
   script: "apps/server/src/index.ts", // Entry point of your application
   interpreter: "bun", // Bun interpreter
   env: {
